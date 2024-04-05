cb1ef393a3e579b0bcbc645aff0b3840e37efb38
AngeloDSAlvarez <angelodaniel1221@gmail.com>
2024-04-04 22:28:47 -0300

    Resolução exercicio 4 da lista 02

---
+++ bk-ap-lista02/bk-ap-lista02/exercicios02/ex04/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int[] vetor1 = {2, 3, 4};
+int[] vetor2 = {2, 3, 4};
+int[] vetorResultado = {0, 0, 0};
+
+
+
+for (int i = 0; i < vetor1.Length; i++)
+{
+    vetorResultado[i] = vetor1[i] + vetor2[i];
+}
+
+
+foreach (int i in vetorResultado)
+{
+    Console.WriteLine(i);
+}
+
