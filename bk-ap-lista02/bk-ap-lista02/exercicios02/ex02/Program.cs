766fcb6863602fc75ab7fee5235468a9b685e14f
AngeloDSAlvarez <angelodaniel1221@gmail.com>
2024-04-04 22:28:27 -0300

    Resolução exercicios 02 da lista 02

---
+++ bk-ap-lista02/bk-ap-lista02/exercicios02/ex02/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int[] numeros = { 4, 2, 12, 4, 10 };
+
+int maiorNumero = numeros[0];
+int menorNumero = numeros[0];
+foreach (int numero in numeros)
+{  
+    if (numero > maiorNumero)
+    {
+        maiorNumero = numero;
+    }
+
+    if (numero < menorNumero)
+    {
+        menorNumero = numero;
+    }
+}
+
+Console.WriteLine("Maior numero: " + maiorNumero + "\nMenor numero: " +  menorNumero);    
