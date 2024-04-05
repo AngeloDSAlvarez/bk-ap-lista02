eafe3ab648c95d98f4e9b14b7f09fc5b3ceeca19
AngeloDSAlvarez <angelodaniel1221@gmail.com>
2024-04-04 22:28:37 -0300

    Resolução exercicio 3 da lista 02

---
+++ bk-ap-lista02/bk-ap-lista02/exercicios02/ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int[,] numeros =
+{
+    {0, 0, 0},
+    {0, 0, 0},
+    {0, 0, 0}
+};
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        numeros[i, j] = Convert.ToInt16(Console.ReadLine());
+    }
+}
+
+Console.Clear();
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
